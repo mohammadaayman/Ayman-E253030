4ba5f135cb3ab150ad37086e779318d43f661169
Mohammad Ayman <mohammadaayman15@gmail.com>
2026-06-18 19:13:04 +0600

    Create MyClass.java

---
+++ LabOne/MyClass.java
@@ -0,0 +1,18 @@
+public class MyClass {
+    public static void main(String[] args) {
+        String name = "Mohammad Ayman";
+        String studentId = "E253030";
+        int age = 20;
+        String email = "aymanctg50@gmail.com";
+        String phone = "0123456788899";
+        String address = "Kulgaon, Oxygen, Chattogram";
+
+        System.out.println("===== Student Information =====");
+        System.out.println("Name : " + name);
+        System.out.println("Student ID : " + studentId);
+        System.out.println("Age : " + age);
+        System.out.println("Email : " + email);
+        System.out.println("Phone : " + phone);
+        System.out.println("Address : " + address);
+    }
+}
